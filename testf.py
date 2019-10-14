45828cb71f465c3c05989ab88469ce94a434b3fa
Nour Boulahcen <nour.boulahcen@gmail.com>
2019-10-14 16:45:56 +0200

    fix fourrier transform

--- testf.py
+++ testf.py
@@ -1,16 +1,35 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from math import pi
+import matplotlib.pyplot as plt
+import scipy.fftpack
+"""
 
-X = np.linspace(0, 10 * pi, 2000)
+# Number of samplepoints
+N = 600
+# sample spacing
+T = 1.0 / 800.0
+x = np.linspace(0.0, N*T, N)
+y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
+yf = scipy.fftpack.fft(y)
+xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+
+fig, ax = plt.subplots()
+ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+plt.show()
+"""
+
+"""
+J'ai trouvé ca sur stack overflow ya aussi une doc pour scipy.fttpack : https://docs.scipy.org/doc/scipy/reference/tutorial/fftpack.html
+Du coup sur le code qu'on avait ca nous donne ca :
+"""
+n = 600
+X = np.linspace(0, 10 * np.pi, n)
 y = []
-
+t = 1.0/800.0
 A = [5, 2, 1/2, 1]
 F = [1, 2, 4, 10]
 
 for a, f in zip(A, F):
-    y.append(a*np.sin(2*pi*f*X))
-
+    y.append(a*np.sin(2*np.pi*f*X))
 
 Y = sum(y)
 
@@ -31,3 +50,12 @@ fig, ax = plt.subplots()
 ax.plot(X, Y)
 
 fig.show()
+
+
+
+yf2 = scipy.fftpack.fft(Y)
+xf2 = np.linspace(0.0, 1.0/(2.0*t), n/2)
+
+fig, ax = plt.subplots()
+ax.plot(xf2, 2.0/n * np.abs(yf2[:n//2]))
+plt.show()
