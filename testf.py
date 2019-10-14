9a947acbb6bf8ec723eb97d60b4f4fab7fd0ea6d
QuentinN42 <quentin.lieumont@gmail.com>
2019-10-14 11:31:52 +0200

    Test fourier

---
+++ testf.py
@@ -0,0 +1,34 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from math import pi
+
+X = np.linspace(0, 10 * pi, 2000)
+y = []
+
+A = [5, 2, 1/2, 1]
+F = [1, 2, 4, 10]
+
+for a,f in zip(A,F):
+    y.append(a*np.sin(2*pi*f*X))
+
+
+Y = sum(y)
+
+
+fig: plt.Figure
+fig, axs = plt.subplots(len(y))
+
+for i, _y in enumerate(y):
+    axs[i].plot(X, _y)
+
+fig.show()
+
+
+
+fig: plt.Figure
+ax: plt.Axes
+fig, ax = plt.subplots()
+
+ax.plot(X, Y)
+
+fig.show()
