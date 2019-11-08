9466481ac394341b508b038128b2de8c2b7de9cf
QuentinN42 <quentin.lieumont@gmail.com>
2019-11-08 15:34:03 +0100

    more data tests

--- data/test.py
+++ data/test.py
@@ -10,20 +10,33 @@ import seaborn as sns
 
 sns.set(style="white")
 
-head = get_json("data/exo/head.json")
-data = pd.read_csv("data/exo/data1.csv")
+head = get_json("exo/head.json")
+data = pd.read_csv("exo/data1.csv")
 
 ys: np.ndarray = data.values[:, 1:]
 x: np.ndarray = np.arange(len(ys[0]))
 
-print(x[:10])
-print(ys[0, :10])
+print(ys.shape)
 
+moy = np.array([y.mean() for y in ys])
+plt.plot(moy, '+k')
+plt.show()
+
+ys = np.delete(ys, moy.argmax(), 0)
+print(ys.shape)
+
+prec = 0
+end = 300
 graph = sns.JointGrid(x, x)
-
-for y in ys[:2, :]:
+for i, y in enumerate(ys[:end]):
+    if int(100*i/end) != prec:
+        prec = int(100*i/end)
+        if prec < 10:
+            print(f"0{prec}%")
+        else:
+            print(f"{prec}%")
     graph.y = y
     graph.plot_joint(plt.scatter, marker="+")
-    graph.plot_marginals(sns.kdeplot, shade=True)
+    graph.plot_marginals(sns.distplot)
 
 plt.show()
